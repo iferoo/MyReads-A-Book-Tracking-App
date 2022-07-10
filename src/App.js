aae65880585f7e02eaa697543f55db2f2c071db5
Eyad Mohamed <eyadmohamed2859@gmail.com>
2022-07-10 13:56:39 +0200

    add router and setup app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import BookPage from './components/BookPage';
+import SearchBooks from './components/SearchBooks';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<BookPage />} />
+      <Route path="search" element={<SearchBooks />} />
+    </Routes>
   );
 }
 
